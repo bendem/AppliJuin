908c3314653e2459a213d8d3e7b56b931b82228a
bendem <bendembd@gmail.com>
2013-04-16 13:35:51 +0200

    Prévention de redirection si en mode debug

--- libs/functions.php
+++ libs/functions.php
@@ -124,6 +124,11 @@ function redirect($url, $code = null) {
  *							 Si faux, renvoie vers la page de connexion
  */
 function kick($connected = false) {
+	if(DEBUG) {
+		session_set_flash('DEBUG : Redirection empêchée (pb de connexion)', 'warning');
+		return;
+	}
+
 	if(session_read('user') == $connected) {
 		if($connected) {
 			$text = "Vous êtes déjà connecté !";
